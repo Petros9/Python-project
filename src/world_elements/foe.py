43de106adc74308d0592f381c67bb5d73718f711
Petros9 <58508596+Petros9@users.noreply.github.com>
2020-05-06 12:05:28 +0200

    Add files via upload

--- src/world_elements/foe.py
+++ src/world_elements/foe.py
@@ -1,37 +1,42 @@
+import pygame
+
+from basic import Point
 from character import Character
 import basic as bs
 from models import Models
-from settings import *
+from settings import HORIZONTAL_ACCELERATION, IMMORTALITY_TIME, FOE_BULLETS_PER_BURST, CELL_SIZE, FOE_RANGE
 
 
 class Foe(Character):
     def __init__(self, img, x, y, level):
         super().__init__(img, bs.Point(x, y), level)
         self.foe_health = 3
+        self.foe_direct = bs.Direction.LEFT
         self.immortality_timer = 0
         self.reload_timer = 0
         self.bullets = FOE_BULLETS_PER_BURST
+        self.landed = False
 
     def take_hit(self):
         self.foe_health -= 1
         self.immortality_timer = IMMORTALITY_TIME
-        if(self.direction is bs.Direction.LEFT):
+        if(self.foe_direct == bs.Direction.LEFT):
             self.image = Models.FOE_L_DAM_IMG
         else:
             self.image = Models.FOE_R_DAM_IMG
 
     def shoot(self):
-        if (self.direction is bs.Direction.RIGHT):
-            bullet_position = bs.Point(self.rect.x + 18, self.rect.y)
-            bullet_velocity = bs.Point(15, 0)
+        if (self.foe_direct == bs.Direction.RIGHT):
+            bullet_position = Point(self.rect.x + 18, self.rect.y)
+            bullet_velocity = Point(15, 0)
             self.world.shoot(bullet_position, bullet_velocity)
         else:
-            bullet_position = bs.Point(self.rect.x - 18, self.rect.y)
-            bullet_velocity = bs.Point(-15, 0)
+            bullet_position = Point(self.rect.x - 18, self.rect.y)
+            bullet_velocity = Point(-15, 0)
             self.world.shoot(bullet_position, bullet_velocity)
 
     def change_image(self):
-        if(self.direction is bs.Direction.RIGHT):
+        if(self.foe_direct == bs.Direction.RIGHT):
             self.image = Models.FOE_R_IMG
         else:
             self.image = Models.FOE_L_IMG
@@ -40,36 +45,38 @@ class Foe(Character):
         if(abs(hero_position.y - self.position.y) > CELL_SIZE*3/4):
             return False
         else:
-            if(self.direction is bs.Direction.LEFT):
+            if(self.foe_direct == bs.Direction.LEFT):
                 return FOE_RANGE > self.position.x - hero_position.x > 0
             else:
                 return 0 < self.position.x - hero_position.x > FOE_RANGE
 
     def reverse_direction(self):
-        if(self.direction is bs.Direction.RIGHT):
-            self.direction = bs.Direction.LEFT
+        if(self.foe_direct == bs.Direction.RIGHT):
+            self.foe_direct = bs.Direction.LEFT
         else:
-            self.direction = bs.Direction.RIGHT
+            self.foe_direct = bs.Direction.RIGHT
 
     def update(self):
 
-        old_point = self.position
-        if(self.direction is bs.Direction.LEFT):
+        old_position = self.position
+        if(self.foe_direct == bs.Direction.LEFT):
             x_direction = -1
 
         else:
             x_direction = 1
-        self.accelerate(x_direction * HORIZONTAL_ACCELERATION / 2, 0)
-        if (not self.ground_detector.is_on_ground()):
-            self.velocity.x = 0
-            self.accelerate(-x_direction * HORIZONTAL_ACCELERATION / 2, 0)
-            self.position.x -= 15 * x_direction
-            self.position.y -= 15
-            self.rect.x -= 15 * x_direction
-            self.rect.y -= 15
-            self.reverse_direction()
+        self.accelerate(x_direction*HORIZONTAL_ACCELERATION/2, 0)
+
+        if(not self.landed):
+            if(self.ground_detector.is_on_ground()):
+                self.landed = True
 
         super().update()
 
-        if (old_point == self.position):
+        if (old_position == self.position):
             self.reverse_direction()
+
+        elif(old_position.y != self.position.y and self.landed):
+            self.reverse_direction()
+            self.position = old_position
+            self.velocity.x = 0
+            self.velocity.y = 0
