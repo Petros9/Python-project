486b670ffccfeb2d945f73be88bc405041c9ac87
Kamilbur <kamil.burkiewicz@gmail.com>
2020-05-06 02:22:32 +0200

    Use baseclass direction. It is less jiggling now :(

--- src/world_elements/foe.py
+++ src/world_elements/foe.py
@@ -8,7 +8,6 @@ class Foe(Character):
     def __init__(self, img, x, y, level):
         super().__init__(img, bs.Point(x, y), level)
         self.foe_health = 3
-        self.foe_direct = bs.Direction.LEFT
         self.immortality_timer = 0
         self.reload_timer = 0
         self.bullets = FOE_BULLETS_PER_BURST
@@ -16,13 +15,13 @@ class Foe(Character):
     def take_hit(self):
         self.foe_health -= 1
         self.immortality_timer = IMMORTALITY_TIME
-        if(self.foe_direct is bs.Direction.LEFT):
+        if(self.direction is bs.Direction.LEFT):
             self.image = Models.FOE_L_DAM_IMG
         else:
             self.image = Models.FOE_R_DAM_IMG
 
     def shoot(self):
-        if (self.foe_direct is bs.Direction.RIGHT):
+        if (self.direction is bs.Direction.RIGHT):
             bullet_position = bs.Point(self.rect.x + 18, self.rect.y)
             bullet_velocity = bs.Point(15, 0)
             self.world.shoot(bullet_position, bullet_velocity)
@@ -32,7 +31,7 @@ class Foe(Character):
             self.world.shoot(bullet_position, bullet_velocity)
 
     def change_image(self):
-        if(self.foe_direct is bs.Direction.RIGHT):
+        if(self.direction is bs.Direction.RIGHT):
             self.image = Models.FOE_R_IMG
         else:
             self.image = Models.FOE_L_IMG
@@ -41,21 +40,21 @@ class Foe(Character):
         if(abs(hero_position.y - self.position.y) > CELL_SIZE*3/4):
             return False
         else:
-            if(self.foe_direct is bs.Direction.LEFT):
+            if(self.direction is bs.Direction.LEFT):
                 return FOE_RANGE > self.position.x - hero_position.x > 0
             else:
                 return 0 < self.position.x - hero_position.x > FOE_RANGE
 
     def reverse_direction(self):
-        if(self.foe_direct is bs.Direction.RIGHT):
-            self.foe_direct = bs.Direction.LEFT
+        if(self.direction is bs.Direction.RIGHT):
+            self.direction = bs.Direction.LEFT
         else:
-            self.foe_direct = bs.Direction.RIGHT
+            self.direction = bs.Direction.RIGHT
 
     def update(self):
 
         old_point = self.position
-        if(self.foe_direct is bs.Direction.LEFT):
+        if(self.direction is bs.Direction.LEFT):
             x_direction = -1
 
         else:
@@ -74,5 +73,3 @@ class Foe(Character):
 
         if (old_point == self.position):
             self.reverse_direction()
-
-
